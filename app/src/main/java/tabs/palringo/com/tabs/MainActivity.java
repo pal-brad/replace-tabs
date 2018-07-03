4be93fc2f802f724aed25ab0fc24940ecf620f92
bradley fail <bradley.fail@palringo.com>
2018-07-03 15:19:01 +0100

    terstd

--- app/src/main/java/tabs/palringo/com/tabs/MainActivity.java
+++ app/src/main/java/tabs/palringo/com/tabs/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		String s = "hi	bye";//tabs
+		String s2 = "hi    bye";//space
 		setContentView(R.layout.activity_main);
 	}
 }
